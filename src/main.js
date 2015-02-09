909bfd40fa5310bb4a1756402737dcd81fd9360b
Charles Arciniega <charlesarcin@gmail.com>
2015-02-09 00:01:26 -0800

    The app view will contain all sub views.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 define(function(require, exports, module) {
-	var Engine  = require('famous/core/Engine');
+  var Engine  = require('famous/core/Engine')
+  var AppView = require('views/AppView')
 
-	var mainContext = Engine.createContext();
+  var mainContext = Engine.createContext()
+
+  // Instantiate appview
+  var appView = new AppView()
+
+  // Mount the appView onto the mainContext
+  mainContext.add(appView)
 });
