66528d8094b5205adae6af3af3604be70312ea22
Charles Arciniega <charlesarcin@gmail.com>
2015-02-11 08:29:05 -0800

    Add camera to the screen to make sense of the gallery

--- src/views/AppView.js
+++ src/views/AppView.js
@@ -6,6 +6,7 @@ define(function(require, exports, module) {
     var Surface = require('famous/core/Surface')
     var Transform = require('famous/core/Transform')
     var StateModifier = require('famous/modifiers/StateModifier')
+    var ImageSurface = require('famous/surfaces/ImageSurface')
 
     var SlideshowView = require('views/SlideshowView')
 
@@ -19,13 +20,34 @@ define(function(require, exports, module) {
         })
 
         this.add(slideshowView)
+
+        _createCamera.call(this)
     }
 
     AppView.prototype = Object.create(View.prototype)
     AppView.prototype.constructor = AppView
 
     AppView.DEFAULT_OPTIONS = {
-      urls: undefined
+      urls: undefined,
+      cameraWidth: 0.6 * window.innerHeight
+    }
+
+    function _createCamera(){
+      var camera = new ImageSurface({
+        size: [this.options.cameraWidth, true],
+        content: 'img/camera.png',
+        properties: {
+          width: '100%'
+        }
+      })
+
+      var cameraModifier = new StateModifier({
+        origin: [0.5, 0],
+        align: [0.5, 0],
+        transform: Transform.behind
+      })
+
+      this.add(cameraModifier).add(camera)
     }
 
     module.exports = AppView
