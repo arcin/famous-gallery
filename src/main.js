0619165ad80e0d9a086856725c47a745f19d2a31
Charles Arciniega <charlesarcin@gmail.com>
2015-02-10 22:23:19 -0800

    Initialize our app once there are image urls for the slideshow

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 define(function(require, exports, module) {
   var Engine  = require('famous/core/Engine')
-  var AppView = require('views/AppView')
-
+  var Utility = require('famous/utilities/Utility')
   var mainContext = Engine.createContext()
 
-  // Instantiate appview
-  var appView = new AppView()
+  var AppView = require('views/AppView')
+  var SlideData = require('data/SlideData')
 
-  // Mount the appView onto the mainContext
-  mainContext.add(appView)
+  // async call that will initialize app
+  Utility.loadURL(SlideData.getUrl(), initApp)
+
+  function initApp(urlData){
+    var urls = SlideData.parse(urlData)
+    var appView = new AppView({ urls: urls })
+
+    // Mount the appView onto the mainContext
+    mainContext.add(appView)
+  }
+
 });
