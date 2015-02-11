69ffde733e8324a22be01d553121b9953d217ab3
Charles Arciniega <charlesarcin@gmail.com>
2015-02-10 22:15:55 -0800

    We need a lightbox to handle multiple photos

--- src/views/SlideshowView.js
+++ src/views/SlideshowView.js
@@ -6,19 +6,34 @@ define(function(require, exports, module) {
     var Surface = require('famous/core/Surface');
     var Transform = require('famous/core/Transform');
     var StateModifier = require('famous/modifiers/StateModifier');
+    var Lightbox = require('famous/views/Lightbox')
 
-    var SlideView = require('views/SlideView')
     function SlideshowView() {
+      View.apply(this, arguments);
 
-        View.apply(this, arguments);
-        var slideView = new SlideView()
-        this.add(slideView)
+      this.rootModifier = new StateModifier({
+        size: this.options.size,
+        origin: [0.5, 0],
+        align: [0.5, 0]
+      })
+
+      this.mainNode = this.add(this.rootModifier)
+
+      _createLightbox.call(this)
     }
 
     SlideshowView.prototype = Object.create(View.prototype);
     SlideshowView.prototype.constructor = SlideshowView;
 
-    SlideshowView.DEFAULT_OPTIONS = {};
+    SlideshowView.DEFAULT_OPTIONS = {
+      size: [450, 500],
+      lightboxOpts: {}
+    };
+
+    function _createLightbox(){
+      this.lightbox = new Lightbox(this.options.lightboxOpts)
+      this.mainNode.add(this.lightbox)
+    }
 
     module.exports = SlideshowView;
 });
