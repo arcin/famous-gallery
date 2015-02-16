bf7798861c1af1845a4537ca53709cc39b88afd5
Charles Arciniega <charlesarcin@gmail.com>
2015-02-15 16:06:45 -0800

    Add animation to make slide swing and look like its developing itself

--- src/views/SlideView.js
+++ src/views/SlideView.js
@@ -7,6 +7,11 @@ define(function(require, exports, module) {
     var Transform = require('famous/core/Transform');
     var StateModifier = require('famous/modifiers/StateModifier');
     var ImageSurface = require('famous/surfaces/ImageSurface')
+    var Transitionable = require('famous/transitions/Transitionable')
+    var SpringTransition = require('famous/transitions/SpringTransition')
+
+    // Register spring transition
+    Transitionable.registerMethod('spring', SpringTransition)
 
     var SlideData = require('data/SlideData')
 
@@ -34,11 +39,34 @@ define(function(require, exports, module) {
     SlideView.prototype = Object.create(View.prototype);
     SlideView.prototype.constructor = SlideView;
 
+    SlideView.prototype.fadeIn = function(){
+      this.photoModifier.setOpacity(1, { duration: 1500, curve: 'easeIn' })
+      this.shake()
+    }
+
+    SlideView.prototype.shake = function(){
+      this.rootModifier.halt()
+
+      // rotate slide back and forth
+      this.rootModifier.setTransform(
+        Transform.rotateX(this.options.angle),
+        { duration: 200, curve: 'easeOut' }
+      )
+
+      // use spring transition to transform slide back to original state.
+      // use a low damping ratio
+      this.rootModifier.setTransform(
+        Transform.identity,
+        { method: 'spring', period: 600, dampingRation: 0.15 }
+      )
+    }
+
     SlideView.DEFAULT_OPTIONS = {
       size: [400, 450],
       filmBorder: 15,
       photoBorder: 3,
-      photoUrl: SlideData.defaultImage
+      photoUrl: SlideData.defaultImage,
+      angle: -0.5
     };
 
     // helper function used to create background surface
@@ -104,7 +132,8 @@ define(function(require, exports, module) {
       this.photoModifier = new StateModifier({
         origin: [0.5, 0],
         align: [0.5, 0],
-        transform: Transform.translate(0, finalBorder, 2)
+        transform: Transform.translate(0, finalBorder, 0.1),
+        opacity: 0.01
       })
 
       this.mainNode.add(this.photoModifier).add(photo)
